8f0570936177aa6108eec46386b38bbc013dda2b
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-13 21:37:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,10 @@ def list_of_directors(source)
   # Write this implementation
   
   list = []
-  
+  index = 0 
+  while index < source.count do 
+    list[index] = source[index][:name]
+  end
   
 end
 
